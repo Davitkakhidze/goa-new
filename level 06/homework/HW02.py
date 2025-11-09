76a828954554092664d33c4ed72c1812def2b042
Davitkakhidze <davitkakhidze46@gmail.com>
2025-11-09 21:44:51 +0400

    second commit

---
+++ level 06/homework/HW02.py
@@ -0,0 +1,10 @@
+#შექმენით 2 ცვლადი და შემდეგ შეადრეთ ერთმანეთზე
+user_num1 = 10
+user_num2 = 20
+print(user_num1<user_num2)
+print(user_num1>user_num2)
+print(user_num1==user_num2)
+print(user_num1<=user_num2)
+print(user_num1>=user_num2)
+print(user_num1!=user_num2)
+
