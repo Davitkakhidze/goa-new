70bc3ce0aa2264ca7799121b405cc43ab89dd6aa
Davitkakhidze <davitkakhidze46@gmail.com>
2025-12-09 18:41:41 +0400

    second commit

---
+++ mini lider helps me.py/Calsswork/CW01.py
@@ -0,0 +1,4 @@
+#გააკეთე 2 ცვლადი და გამოიტანე პირველს + სიცარიელე + მეორე ტერმინალში
+user_number = 9
+user_number1 = 12
+print(user_number + user_number1)
---
+++ mini lider helps me.py/Calsswork/CW03.py
@@ -0,0 +1,8 @@
+#გააკეთე ფუნქცია რომელსაც ექნება integer და string 
+#და მერე გამოიძახე ფუნქცია რაშიც integer იქნება 50 და string იქნება "GOA BEST"
+def number(davit,nika):
+    print(davit + nika)
+
+
+
+number(123,321)
